591c71b4d54bfd629626bafc9fb4ac6d8b6d6b8b
vishantjurish <vishant.juris@gmail.com>
2025-05-05 12:13:51 +0530

    navbar

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FaAngleDown, FaAngleRight, FaBars } from "react-icons/fa";
 import logo from "../../assets/hero/logo/walshenimmigration.webp";
-
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
@@ -19,19 +18,13 @@ const Navbar = () => {
   return (
     <header className="bg-white shadow-md py-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 flex items-center justify-between">
-        {/* Logo */}
         <NavLink to="/" className="flex items-center space-x-2">
-          <img
-            src={logo}
-            alt="Walshken Immigration Logo"
-            className="w-20 h-auto"
-          />
+          <img src={logo} alt="" className="text-red-600 text-2xl" />
           <h1 className="text-xl font-bold">
             Walshken<span className="text-red-600">Immigration</span>
           </h1>
         </NavLink>
 
-        {/* Desktop Menu */}
         <nav className="hidden lg:flex space-x-6 items-center">
           {menuItems.map((item, index) => (
             <div key={index} className="relative">
@@ -49,9 +42,7 @@ const Navbar = () => {
           ))}
         </nav>
 
-        {/* Buttons */}
         <div className="flex items-center space-x-4">
-          {/* Get A Quote - Only show on large screens */}
           <button
             onClick={() => navigate("/contact")}
             className="hidden lg:flex bg-red-600 hover:bg-gray-700 text-white font-semibold text-sm px-5 py-3 rounded-md items-center space-x-2"
@@ -59,7 +50,6 @@ const Navbar = () => {
             <span>Get A Quote</span>
           </button>
 
-          {/* Mobile Menu Toggle Button */}
           <button
             className="lg:hidden text-2xl"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -69,7 +59,6 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile Menu */}
       {menuOpen && (
         <div className="lg:hidden bg-white shadow-md mt-2 px-4 pb-4">
           {menuItems.map((item, index) => (
